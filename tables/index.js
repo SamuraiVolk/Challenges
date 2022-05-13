835d864ebad413e45830b829da0bc88d0c958964
Tony Montoya <zero_14.4@hotmail.com>
2022-05-12 21:10:56 -0700

    initial commit

---
+++ tables/index.js
@@ -0,0 +1,21 @@
+function print(message) {
+    console.log(message);
+}
+
+// Esta es una tabla del 1 al 10
+for (y=0;y < 11; y=y+1){;
+print(`TABLA DEL ${y}`)
+for (x=0;x < 11; x=x+1) {
+    print(`${y} * ${x} is equal to: ${y * x}`)
+    
+}};
+
+// for (x = 1; x < 11; x=x+1) {
+//     var y = 1;
+//     while (y < 11) {
+//         print(`${x} * ${y} is equal to: ${y * x}`)
+
+//         y = y + 1;
+//     }
+// }
+
